231f07b5db7a5526d58203a88ee11a9064a41c28
Abdelrahman Abdallah <abdoelsayed2016@gmail.com>
2026-01-12 16:35:15 +0100

    edit

--- static/js/index.js
+++ static/js/index.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
+
+  $('.tabs li').on('click', function() {
+    var tab = $(this).data('tab');
+
+    $('.tabs li').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    $('.tab-content').hide();
+    $('#' + tab).show();
+  });
 });
